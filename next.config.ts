a30519ea9e3dcfefcd567ff19021f866b9db1a11
StarkAdriano <linkedinajn@gmail.com>
2025-05-30 18:47:00 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,18 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home", // Ajuste para a página correta
+        permanent: true,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
